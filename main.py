86b208275e2c8f01a461f5e44437093173c847d4
Sina <sinarashidiazar@hotmail.com>
2021-03-17 07:23:23 +0330

    sina

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import socket
 for x in range(20,443):
      a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
      #   AF_INET = IP V4 ---- AF_INET6 (IPv6 protocol)
+     # SOCK_STREAM: TCP(reliable, connection oriented)
+     # SOCK_DGRAM: UDP(unreliable, connectionless)
+
+
+
      location = ("135.125.161.63",x)
      result_of_check = a_socket.connect_ex(location)
      if result_of_check == 0:
